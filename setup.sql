6e14672d7ca855f0d568184cc5699a5af81d4715
mika-txt <daloisio.mika.l@gmail.com>
2024-10-17 20:44:26 -0400

    Added https urls

--- setup.sql
+++ setup.sql
@@ -34,52 +34,52 @@ CREATE TABLE IF NOT EXISTS comment (
   PRIMARY KEY (site_url, comment)
 );
 
-INSERT INTO website VALUES ("MYSQL", "mysql.com");
-INSERT INTO user_info VALUES ("mysql.com", "Mika", "D'Aloisio", "mika-txt", "mika@example.com");
-INSERT INTO site_password (site_url, my_password) VALUES ("mysql.com", AES_ENCRYPT("b[!:s6W%=M", @key_str, @init_vector));
-INSERT INTO comment VALUES ("mysql.com", "");
+INSERT INTO website VALUES ("MYSQL", "https://mysql.com");
+INSERT INTO user_info VALUES ("https://mysql.com", "Mika", "D'Aloisio", "mika-txt", "mika@example.com");
+INSERT INTO site_password (site_url, my_password) VALUES ("https://mysql.com", AES_ENCRYPT("b[!:s6W%=M", @key_str, @init_vector));
+INSERT INTO comment VALUES ("https://mysql.com", "");
 
-INSERT INTO website VALUES ("University of Hartford", "hartford.edu");
-INSERT INTO user_info VALUES ("hartford.edu", "Mika", "D'Aloisio", "mika-txt", "mika@example.com");
-INSERT INTO site_password (site_url, my_password) VALUES ("hartford.edu", AES_ENCRYPT("WpaC6_{/4d", @key_str, @init_vector));
-INSERT INTO comment VALUES ("hartford.edu", "");
+INSERT INTO website VALUES ("University of Hartford", "https://hartford.edu");
+INSERT INTO user_info VALUES ("https://hartford.edu", "Mika", "D'Aloisio", "mika-txt", "mika@example.com");
+INSERT INTO site_password (site_url, my_password) VALUES ("https://hartford.edu", AES_ENCRYPT("WpaC6_{/4d", @key_str, @init_vector));
+INSERT INTO comment VALUES ("https://hartford.edu", "");
 
-INSERT INTO website VALUES ("GitHub", "github.com");
-INSERT INTO user_info VALUES ("github.com", "Mika", "D'Aloisio", "mika-txt", "mika@example.com");
-INSERT INTO site_password (site_url, my_password) VALUES ("github.com", AES_ENCRYPT("E.5,?/mVN]", @key_str, @init_vector));
-INSERT INTO comment VALUES ("github.com", "");
+INSERT INTO website VALUES ("GitHub", "https://github.com");
+INSERT INTO user_info VALUES ("https://github.com", "Mika", "D'Aloisio", "mika-txt", "mika@example.com");
+INSERT INTO site_password (site_url, my_password) VALUES ("https://github.com", AES_ENCRYPT("E.5,?/mVN]", @key_str, @init_vector));
+INSERT INTO comment VALUES ("https://github.com", "");
 
-INSERT INTO website VALUES ("Microsoft Office", "office.com");
-INSERT INTO user_info VALUES ("office.com", "Mika", "D'Aloisio", "mika-txt", "mika@example.com");
-INSERT INTO site_password (site_url, my_password) VALUES ("office.com", AES_ENCRYPT("pZ4v@/A!-^", @key_str, @init_vector));
-INSERT INTO comment VALUES ("office.com", "");
+INSERT INTO website VALUES ("Microsoft Office", "https://office.com");
+INSERT INTO user_info VALUES ("https://office.com", "Mika", "D'Aloisio", "mika-txt", "mika@example.com");
+INSERT INTO site_password (site_url, my_password) VALUES ("https://office.com", AES_ENCRYPT("pZ4v@/A!-^", @key_str, @init_vector));
+INSERT INTO comment VALUES ("https://office.com", "");
 
-INSERT INTO website VALUES ("Google", "google.com");
-INSERT INTO user_info VALUES ("google.com", "Mika", "D'Aloisio", "mika-txt", "mika@example.com");
-INSERT INTO site_password (site_url, my_password) VALUES ("google.com", AES_ENCRYPT("pZ4v@/A!-^", @key_str, @init_vector));
-INSERT INTO comment VALUES ("google.com", "");
+INSERT INTO website VALUES ("Google", "https://google.com");
+INSERT INTO user_info VALUES ("https://google.com", "Mika", "D'Aloisio", "mika-txt", "mika@example.com");
+INSERT INTO site_password (site_url, my_password) VALUES ("https://google.com", AES_ENCRYPT("pZ4v@/A!-^", @key_str, @init_vector));
+INSERT INTO comment VALUES ("https://google.com", "");
 
-INSERT INTO website VALUES ("X", "x.com");
-INSERT INTO user_info VALUES ("x.com", "Mika", "D'Aloisio", "mika-txt", "mika@example.com");
-INSERT INTO site_password (site_url, my_password) VALUES ("x.com", AES_ENCRYPT("ejgUZz+-7R", @key_str, @init_vector));
-INSERT INTO comment VALUES ("x.com", "");
+INSERT INTO website VALUES ("X", "https://x.com");
+INSERT INTO user_info VALUES ("https://x.com", "Mika", "D'Aloisio", "mika-txt", "mika@example.com");
+INSERT INTO site_password (site_url, my_password) VALUES ("https://x.com", AES_ENCRYPT("ejgUZz+-7R", @key_str, @init_vector));
+INSERT INTO comment VALUES ("https://x.com", "");
 
-INSERT INTO website VALUES ("Linkedin", "linkedin.com");
-INSERT INTO user_info VALUES ("linkedin.com", "Mika", "D'Aloisio", "mika-txt", "mika@example.com");
-INSERT INTO site_password (site_url, my_password) VALUES ("linkedin.com", AES_ENCRYPT("Ey3w8gW_dp", @key_str, @init_vector));
-INSERT INTO comment VALUES ("linkedin.com", "");
+INSERT INTO website VALUES ("Linkedin", "https://linkedin.com");
+INSERT INTO user_info VALUES ("https://linkedin.com", "Mika", "D'Aloisio", "mika-txt", "mika@example.com");
+INSERT INTO site_password (site_url, my_password) VALUES ("https://linkedin.com", AES_ENCRYPT("Ey3w8gW_dp", @key_str, @init_vector));
+INSERT INTO comment VALUES ("https://linkedin.com", "");
 
-INSERT INTO website VALUES ("Discord", "discord.com");
-INSERT INTO user_info VALUES ("discord.com", "Mika", "D'Aloisio", "mika-txt", "mika@example.com");
-INSERT INTO site_password (site_url, my_password) VALUES ("discord.com", AES_ENCRYPT("F}P7ubAp/+", @key_str, @init_vector));
-INSERT INTO comment VALUES ("discord.com", "");
+INSERT INTO website VALUES ("Discord", "https://discord.com");
+INSERT INTO user_info VALUES ("https://discord.com", "Mika", "D'Aloisio", "mika-txt", "mika@example.com");
+INSERT INTO site_password (site_url, my_password) VALUES ("https://discord.com", AES_ENCRYPT("F}P7ubAp/+", @key_str, @init_vector));
+INSERT INTO comment VALUES ("https://discord.com", "");
 
-INSERT INTO website VALUES ("Apple", "apple.com");
-INSERT INTO user_info VALUES ("apple.com", "Mika", "D'Aloisio", "mika-txt", "mika@example.com");
-INSERT INTO site_password (site_url, my_password) VALUES ("apple.com", AES_ENCRYPT("j/J(7Np;+)", @key_str, @init_vector));
-INSERT INTO comment VALUES ("apple.com", "");
+INSERT INTO website VALUES ("Apple", "https://apple.com");
+INSERT INTO user_info VALUES ("https://apple.com", "Mika", "D'Aloisio", "mika-txt", "mika@example.com");
+INSERT INTO site_password (site_url, my_password) VALUES ("https://apple.com", AES_ENCRYPT("j/J(7Np;+)", @key_str, @init_vector));
+INSERT INTO comment VALUES ("https://apple.com", "");
 
-INSERT INTO website VALUES ("PayPal", "paypal.com");
-INSERT INTO user_info VALUES ("paypal.com", "Mika", "D'Aloisio", "mika-txt", "mika@example.com");
-INSERT INTO site_password (site_url, my_password) VALUES ("paypal.com", AES_ENCRYPT("f6(SU;gJ*h", @key_str, @init_vector));
-INSERT INTO comment VALUES ("paypal.com", "");
+INSERT INTO website VALUES ("PayPal", "https://paypal.com");
+INSERT INTO user_info VALUES ("https://paypal.com", "Mika", "D'Aloisio", "mika-txt", "mika@example.com");
+INSERT INTO site_password (site_url, my_password) VALUES ("https://paypal.com", AES_ENCRYPT("f6(SU;gJ*h", @key_str, @init_vector));
+INSERT INTO comment VALUES ("https://paypal.com", "");
